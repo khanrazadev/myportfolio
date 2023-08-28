75d3c1fcd1e5f2e64eba25ac361bb12774163dd1
raza <razakhanha05@gmail.com>
2023-08-28 15:40:58 +0530

    Added navbar Added hero Added about

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter className="font-mono">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
--- src/constants/index.js
+++ src/constants/index.js
@@ -42,7 +42,7 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Frontend Developer",
     icon: web,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Blockchain Developer",
     icon: creator,
   },
 ];
