5e6af3bd48e5c8aa6e72f27744b767c2fe8e337b
raza <razakhanha05@gmail.com>
2023-08-30 20:38:16 +0530

    emv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import { styles } from "../styles";
 import { Link } from "react-router-dom";
 import { navLinks } from "../constants/index";
-import { logo, menu, close } from "../assets";
+import { menu, close } from "../assets";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Navbar = () => {
   const [active, setActive] = useState();
@@ -26,19 +27,24 @@ const Navbar = () => {
             Raza Khan
           </p>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((nav) => (
-            <li
-              key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
+        <div className="flex gap-4">
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((nav) => (
+              <li
+                key={nav.id}
+                className={`${
+                  active === nav.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="list-none hidden sm:flex justify-between w-12 mt-1 text-[#915EFF] animate-pulse cursor-pointer z-50">
+            <SocialLinks />
+          </div>
+        </div>
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
@@ -67,6 +73,9 @@ const Navbar = () => {
                   <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
+              <div className=" flex gap-5 text-[#915EFF] animate-pulse cursor-pointer">
+                <SocialLinks />
+              </div>
             </ul>
           </div>
         </div>
@@ -75,4 +84,21 @@ const Navbar = () => {
   );
 };
 
+const SocialLinks = () => (
+  <>
+    <BsGithub
+      onClick={() =>
+        window.open("https://github.com/razakhan05?tab=repositories", "_blank")
+      }
+    />
+    <BsLinkedin
+      onClick={() =>
+        window.open(
+          "https://www.linkedin.com/in/raza-khan-109262247/",
+          "_blank"
+        )
+      }
+    />
+  </>
+);
 export default Navbar;
