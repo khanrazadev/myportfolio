9f624c8762864e91480939bac756f549561aa0b0
raza <razakhanha05@gmail.com>
2023-08-30 18:28:52 +0530

    Fixed tilt error

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
